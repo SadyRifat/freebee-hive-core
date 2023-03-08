8b66978418154a9945a514063e7b88e6f1230348
SadyRifat <sadyrifat@gmail.com>
2023-03-08 19:01:04 +0600

    initial commit

---
+++ src/test/java/com/freebee/hive/FreebeeHiveCoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.freebee.hive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FreebeeHiveCoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
